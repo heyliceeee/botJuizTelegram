f16200f98bdfe110cb7a17fe77cd7a81cc48cc54
8200118 <8200118@estg.ipp.pt>
2023-05-23 16:18:04 +0100

    output from user

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
 import java.util.List;
 
@@ -36,7 +38,10 @@ public class Bot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+        Message msg = update.getMessage(); //mensagem
+        User user = msg.getFrom(); //nome do utilizador
+
+        System.out.println(user.getFirstName() + " enviou: " + msg.getText());
     }
 
     @Override
